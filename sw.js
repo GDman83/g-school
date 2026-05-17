fd86282b5c2c509726f8e1c96357932f41fbc47e
GDman83 <55540045+GDman83@users.noreply.github.com>
2026-05-17 10:32:34 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,7 +23,6 @@ self.addEventListener('activate', (event) => {
 
 // 3. 🎯【網絡攔截優化】教材走綠色通道，不進行頑固死快取，確保 data.json 隨更隨新
 self.addEventListener('fetch', (event) => {
-    // 讓所有請求直接走網路索取最新資料，如果網路斷開，再嘗試抓基本骨架
     event.respondWith(
         fetch(event.request).catch(() => {
             return caches.match(event.request);
